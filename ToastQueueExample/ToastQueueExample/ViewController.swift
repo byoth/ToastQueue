0fdf588d6fd1a8e84203260305eb8e3bd4ea14b6
Byoth <taehyunjo97@gmail.com>
2019-12-16 16:05:40 +0900

    Update example

--- ToastQueueExample/ToastQueueExample/ViewController.swift
+++ ToastQueueExample/ToastQueueExample/ViewController.swift
@@ -13,17 +13,38 @@ import ToastQueue
 
 final class ViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
-    @IBOutlet weak var button: UIButton!
     
     
-    fileprivate var count: Int = 0
+    private var _toastCount: Int = 0
     
     
-    @IBAction func buttonDidTap(_ sender: Any) {
-        self.count += 1
-        
-        ToastManager.shared.showMessage("\(self.textField.text ?? "")\(self.count)")
+    var currentToastMessage: String {
+        return "\(self.textField.text ?? "")\(self._toastCount)"
+    }
+    
+    
+    @IBAction func toastButtonDidTap(_ sender: Any) {
+        self.showToastToTest()
         
         self.view.endEditing(true)
+    }
+    
+    @IBAction func sceneButtonDidTap(_ sender: Any) {
+        self.presentScene()
+    }
+    
+    
+    fileprivate func showToastToTest() {
+        self._toastCount += 1
+        
+        ToastManager.shared.showMessage(self.currentToastMessage)
+    }
+    
+    fileprivate func presentScene() {
+        guard let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController() as? ViewController else {
+            return
+        }
+        
+        self.present(viewController, animated: true)
     }
 }
